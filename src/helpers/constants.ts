a5a690ad5e6add9769d182dfc8de664b00cec688
Hameeth Asmath S <hameethasmaths@SSLAP0390.local>
2024-05-02 12:00:59 +0530

    constants file added

---
+++ src/helpers/constants.ts
@@ -0,0 +1,36 @@
+export const rolesOptions = [
+  "Frontend",
+  "Backend",
+  "Fullstack",
+  "IOS",
+  "Android",
+  "Tech Lead",
+  "Web3",
+  "ML",
+];
+
+export const experienceOptions = [
+  "1",
+  "2",
+  "3",
+  "4",
+  "5",
+  "6",
+  "7",
+  "8",
+  "9",
+  "10",
+];
+
+export const modeOfWorkOptions = ["Remote", "Hybrid", "Inoffice"];
+
+export const basePayOptions = [
+    "0L",
+    "10L",
+    "20L",
+    "30L",
+    "40L",
+    "50L",
+    "60L",
+    "70L"
+]
